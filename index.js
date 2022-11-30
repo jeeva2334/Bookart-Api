d4faf1af6c48bac0491c2336775d0f7cb0482e96
jeeva <101379886+sanjay2334@users.noreply.github.com>
2022-11-30 19:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,9 @@ const {
   deleteBook
 } = require('./Controller/Books.controller') 
 
-app.use(cors());
-
-
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
